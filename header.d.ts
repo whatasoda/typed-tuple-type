e79e64dd9bdc83570628cceb0d215d7a37c15f6b
whatasoda <git@whatasoda.me>
2019-11-10 19:15:08 +0900

    fix: remove "length"

--- header.d.ts
+++ header.d.ts
@@ -1,18 +1,14 @@
 /* header.d.ts */
-type Placeholder = Omit<[[]], keyof any[]>;
+type Placeholder = Omit<[[], [0]], keyof any[]>;
 interface Tuples extends Placeholder {}
 
-type KeyToPick = Exclude<keyof Tuples, keyof any[]>;
 type Numerify<T extends any[]> = { [I in keyof T]: number };
-
-type NumTuplesTmp<T extends Record<KeyToPick, any[]>> = {
-  [I in keyof T]: T[I] extends number[] ? Pick<Numerify<T[I]>, Extract<keyof T[I], KeyToPick> | 'length'> : [];
+export type NumericTuples = {
+  [I in Exclude<keyof Tuples, keyof any[]>]: Omit<Numerify<Tuples[I]>, keyof any[]>;
 };
-
-export interface NumericTuples extends Omit<NumTuplesTmp<Tuples>, keyof any[]> {}
-export type TupleLength = NumericTuples[keyof NumericTuples]['length'];
-export type S2N<T extends keyof NumericTuples> = NumericTuples[T]['length'];
+export type TupleLength = Tuples[keyof NumericTuples]['length'];
+export type S2N<T extends keyof NumericTuples> = Tuples[T]['length'];
 export type N2S<T extends TupleLength> = {
-  [K in keyof NumericTuples]: NumericTuples[K]['length'] extends T ? K : never;
+  [K in keyof NumericTuples]: Tuples[K]['length'] extends T ? K : never;
 }[keyof NumericTuples];
 /* End of header.d.ts */
